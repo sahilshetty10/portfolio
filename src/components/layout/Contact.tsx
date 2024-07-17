e8d8b2318651dc17e13f61624528034554e15261
Sahil Shetty <sahilshetty69@gmail.com>
2024-07-16 22:08:45 -0400

    fixed imports

--- src/components/layout/Contact.tsx
+++ src/components/layout/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import Title from "../ui/Title";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
